631ae8af102c64eca34d20a106f89d53581e2997
yakisugita <yakisugita@gmail.com>
2025-03-04 18:38:40 +0900

    Admin用管理画面

---
+++ Controller/Admin/TinySecureAdminController.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+// namespace TinySecure\Controller\Admin;
+
+class TinySecureController extends \BaserCore\Controller\Admin\BcAdminAppController
+{
+    public function index()
+    {
+        // 
+    }
+}
